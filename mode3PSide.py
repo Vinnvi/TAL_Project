7d35c6c2a1df6732658aa2ea30b03a9ca556f5f6
Vincent Doisneau <vin.doisneau@free.fr>
2018-04-26 16:04:53 +0200

    Is it a ? + check conjugaison

--- mode3PSide.py
+++ mode3PSide.py
@@ -24,7 +24,7 @@ def boucle(classeToGuess,animalToGuess):
 	else :
 		print(answer)
 		boucle(classeToGuess,animalToGuess)
-	
+
 
 def decomposition_sentence(mots,classeToGuess,animalToGuess):
 
@@ -39,9 +39,56 @@ def decomposition_sentence(mots,classeToGuess,animalToGuess):
 		elif mots[0] in auxiliary_n :
 			1
 		elif mots[0] in be_verbs :
-			1
+			if mots[1] in subject :
+				if checkConj(mots[1],mots[2]):
+					if mots[2] in a :
+						if mots[3] in modeAnimal.colors :
+							myAnimal = databaseAnimals.animals.get(classeToGuess).get(animalToGuess)
+							myColors = myAnimal.get("colors")
+							for c in myColors :
+								if mots[3] == c :
+									return "Yes, it is "+mots[3]
+							return "No, it is not "+mots[3]
+						else :
+							for classeA in list(databaseAnimals.animals.keys()) :
+								for an in list(databaseAnimals.animals.get(classeA).keys()) :
+									print(an)
+									if an == animalToGuess :
+										return "ok"
+							#don't manage h case yet (unpredictible)
+							if mots[3][0] == "a" or mots[3][0] == "u" or mots[3][0] == "e" or mots[3][0] == "y" or mots[3][0] == "i" or mots[3][0] == "o":
+								return "No, it's not an "+mots[3]
+							else :
+								return "No it's not a "+mots[3]
 		elif mots[0] in be_verbs_n :
 			1
+		elif mots[0] in su_be :
+			if mots[1] in a :
+				if mots[2] in modeAnimal.colors :
+					myAnimal = databaseAnimals.animals.get(classeToGuess).get(animalToGuess)
+					myColors = myAnimal.get("colors")
+					for c in myColors :
+						if mots[2] == c :
+							return "Yes, it is "+mots[2]
+					return "No, it is not "+mots[2]
+				else :
+					for classeA in list(databaseAnimals.animals.keys()) :
+						for an in list(databaseAnimals.animals.get(classeA).keys()) :
+							print(an)
+							if an == animalToGuess :
+								return "ok"
+					#don't manage h case yet (unpredictible)
+					if mots[2][0] == "a" or mots[2][0] == "u" or mots[2][0] == "e" or mots[2][0] == "y" or mots[2][0] == "i" or mots[2][0] == "o":
+						return "No, it's not an "+mots[2]
+					else :
+						return "No it's not a "+mots[2]
+			elif mots[1] in modeAnimal.colors :
+				myAnimal = databaseAnimals.animals.get(classeToGuess).get(animalToGuess)
+				myColors = myAnimal.get("colors")
+				for c in myColors :
+					if mots[2] == c :
+						return "Yes, it is "+mots[2]
+				return "No, it is not "+mots[2]
 
 		elif mots[0] in subject :
 			if mots[1] in be_verbs :
@@ -95,6 +142,7 @@ def decomposition_sentence(mots,classeToGuess,animalToGuess):
 auxiliary_n = ["don't","can't","couldn't"]
 auxiliary = ["did","does","do","can","could"]
 subject = ["animal","it","he","she","they","i"]
+su_be = ["i'm","it's","you're","we're","they're","he's","she's"]
 be_verbs = ["was","is","will","were","am","are","i'm","it's"]
 be_verbs_n = ["isn't","aren't","wasn't"]
 s_be_verbs = ["i'm","it's"]
@@ -113,3 +161,23 @@ eat = ["eat","eating","eats","ate","eaten"]
 action_verbs = [see,live,fly,swim,crawl,eat]
 t = ["ever","never","already"]
 location = ["grassland","city","water","savannah","forest","jungle","river","mountain","mountains","desert","antartic","sea","floe"]
+
+#disable you + is possibility for example (and check 's' for 3rd person)
+#aim subject + be/have
+def checkConj(subject,verb):
+	if subject == "i" :
+		if verb == "am" or verb == "have":
+			return True
+		else :
+			return False
+	if subject == "you" or subject == "we" or subject == "they" :
+		if verb == "are" or verb == "have" :
+			return True
+		else :
+			return False
+	if subject == "it" or subject == "he" or subject "she":
+		if verb == "is" or verb == "has" :
+			return True
+		else :
+			return False
+	return False
